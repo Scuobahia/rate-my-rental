8fbd2ff06c896431f88011364f4cb3fd1a6f4ec4
ParkerC1999 <parkman.christiansen@gmail.com>
2021-07-29 19:54:41 -0600

    routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,12 @@
+const router = require('express');
+const { Comment } = require('../../model');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
