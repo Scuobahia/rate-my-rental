6ae7ae9364666c90f697603ff6b9409837ea5118
Aloha <utah.clayton@gmail.com>
2021-08-02 12:34:37 -0700

    test

--- models/Tenant.js
+++ models/Tenant.js
@@ -2,18 +2,14 @@ const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-// create our Tenant model
 class Tenant extends Model { 
-  // set up method to run on instance data (per Tenant) to check password
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-// define table columns and configuration
 Tenant.init(
   {
-    // TABLE COLUMN DEFINITIONS GO HERE
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -27,9 +23,7 @@ Tenant.init(
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      // there cannot be any duplicate email values in this table
       unique: true,
-      // if allowNull is set to false, we can run our data through validators before creating the table data
       validate: {
         isEmail: true
       }
@@ -44,12 +38,10 @@ Tenant.init(
   },
   {
     hooks: {
-      // set up beforeCreate lifecycle "hook" functionality
       async beforeCreate(newTenantData) {
         newTenantData.password = await bcrypt.hash(newTenantData.password, 10);
         return newTenantData;
       },
-      // set up beforeUpdate lifecycle "hook" functionality
       async beforeUpdate(updatedTenantData) {
         updatedTenantData.password = await bcrypt.hash(updatedTenantData.password, 10);
         return updatedTenantData;
