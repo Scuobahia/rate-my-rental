01cd3a3ddf8d103ed1f6b99ecf0deb48b4c6b476
ParkerC1999 <parkman.christiansen@gmail.com>
2021-08-03 19:44:48 -0600

    get server fixed

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const app = express();
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const sess = {
    secret: 'Super secret secret',
    cookie: {},
