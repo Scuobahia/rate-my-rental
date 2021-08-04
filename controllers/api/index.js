b0b69ed73659e619012e9dac9672cab4851fa96a
Brandon Evans <brandon.evans0708@gmail.com>
2021-08-03 21:01:07 -0600

    added prop js file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,13 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
+const propPage = require('./property-route');
+const landlordPage = require('./landlord-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('/properties', propPage);
+router.use('/landlords', landlordPage)
 
 module.exports = router;
