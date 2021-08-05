55276706f578728098e1073f7e43779ce17ecc5b
Brandon Evans <brandon.evans0708@gmail.com>
2021-08-04 21:34:04 -0600

    hands in the air

--- models/Properties.js
+++ models/Properties.js
@@ -4,78 +4,79 @@ const sequelize = require('../config/connection');
 
 class Properties extends Model { }
 
-Properties.init({
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    number_of_bedrooms: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    number_of_bathrooms: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-    },
-    property_type: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    square_footage: {
-      type: DataTypes.INTERGER,
-      allowNull: false,
-    },
-    year_built: {
-      type: DataTypes.INTERGER,
-      allowNull: false,
-    },
-    location_address: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    location_city: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    location_state: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    zip_code: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    parking_type: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    parking_spaces: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    homeowners_association: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    hoa_fee: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    utilities: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    rent_ammount: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'properties'
-    }
+Properties.init(
+   {
+      id: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         primaryKey: true,
+         autoIncrement: true
+      },
+      number_of_bedrooms: {
+         type: DataTypes.INTEGER,
+         allowNull: false
+      },
+      number_of_bathrooms: {
+         type: DataTypes.DECIMAL,
+         allowNull: false,
+      },
+      property_type: {
+         type: DataTypes.STRING,
+         allowNull: false,
+      },
+      // square_footage: {
+      //    type: DataTypes.INTERGER,
+      //    allowNull: false,
+      // },
+      // year_built: {
+      //    type: DataTypes.INTERGER,
+      //    allowNull: false,
+      // },
+      location_address: {
+         type: DataTypes.STRING,
+         allowNull: false,
+      },
+      // location_city: {
+      //    type: DataTypes.STRING,
+      //    allowNull: false,
+      // },
+      // location_state: {
+      //    type: DataTypes.STRING,
+      //    allowNull: false,
+      // },
+      // zip_code: {
+      //    type: DataTypes.INTEGER,
+      //    allowNull: false,
+      // },
+      // parking_type: {
+      //    type: DataTypes.STRING,
+      //    allowNull: true,
+      // },
+      // parking_spaces: {
+      //    type: DataTypes.STRING,
+      //    allowNull: true,
+      // },
+      // homeowners_association: {
+      //    type: DataTypes.STRING,
+      //    allowNull: false,
+      // },
+      // hoa_fee: {
+      //    type: DataTypes.INTEGER,
+      //    allowNull: false,
+      // },
+      // utilities: {
+      //    type: DataTypes.STRING,
+      //    allowNull: false,
+      // },
+      //   rent_ammount: {
+      //       type: DataTypes.STRING,
+      //       allowNull: false
+      //    }, 
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: "properties"
+   }
 );
 
 module.exports = Properties;
--- models/index.js
+++ models/index.js
@@ -8,75 +8,75 @@ const Reply = require('./Reply');
 
 // create associations
 Landlord.hasMany(Properties, {
-    foreignKey: 'landlord_id'
+   foreignKey: 'properties_id'
 });
 
 Properties.belongsTo(Landlord, {
-    foreignKey: 'landlord_id'
+   foreignKey: 'landlord_id'
 });
 
 Tenant.hasMany(Post, {
-  foreignKey: 'tenant_id'
+   foreignKey: 'tenant_id'
 });
 
 Post.belongsTo(Tenant, {
-  foreignKey: 'tenant_id'
+   foreignKey: 'tenant_id'
 });
 
 Tenant.belongsToMany(Post, {
-  through: Rating,
-//   as: '',
-  foreignKey: 'tenant_id'
+   through: Rating,
+   //   as: '',
+   foreignKey: 'tenant_id'
 });
 
 Post.belongsToMany(Tenant, {
-  through: Rating,
-//   as: 'ratings',
-  foreignKey: 'post_id'
+   through: Rating,
+   //   as: 'ratings',
+   foreignKey: 'post_id'
 });
 
 Rating.belongsTo(Tenant, {
-  foreignKey: 'tenant_id'
+   foreignKey: 'tenant_id'
 });
 
 Rating.belongsTo(Post, {
-  foreignKey: 'post_id'
+   foreignKey: 'post_id'
 });
 
 Tenant.hasMany(Rating, {
-  foreignKey: 'tenant_id'
+   foreignKey: 'tenant_id'
 });
 
 Post.hasMany(Rating, {
-  foreignKey: 'post_id'
+   foreignKey: 'post_id'
 });
 
 Comment.belongsTo(Tenant, {
-  foreignKey: 'tenant_id'
+   foreignKey: 'tenant_id'
 });
 
 Comment.belongsTo(Post, {
-  foreignKey: 'post_id'
+   foreignKey: 'post_id'
 });
 
 Reply.belongsTo(Landlord, {
-    foreignKey: 'landlord_id'
+   foreignKey: 'landlord_id'
 });
 
 Reply.belongsTo(Post, {
-    foreignKey: 'post_id'
+   foreignKey: 'post_id'
 });
 
 Landlord.hasMany(Reply, {
-    foreignKey: 'landlord_id'
+   foreignKey: 'landlord_id'
 });
 
 Tenant.hasMany(Comment, {
-  foreignKey: 'tenant_id'
+   foreignKey: 'tenant_id'
 });
 
 Post.hasMany(Comment, {
-  foreignKey: 'post_id'
+   foreignKey: 'post_id'
 });
 
 module.exports = { Tenant, Landlord, Post, Rating, Comment, Properties, Reply };
