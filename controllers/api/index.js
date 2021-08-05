fe551ae7f26939441cc7694a5af819afa08afd0e
Brandon Evans <brandon.evans0708@gmail.com>
2021-08-04 20:35:44 -0600

    removed bad file from git cache

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,13 +3,13 @@ const router = require('express').Router();
 const tenantRoutes = require('./tenant-routes.js');
 // const postRoutes = require('./post-routes');
 // const commentRoutes = require('./comment-routes');
-// const propPage = require('./property-route');
+const propPage = require('./property-route');
 // const landlordPage = require('./landlord-routes');
 
 router.use('/tenants', tenantRoutes);
 // router.use('/posts', postRoutes);
 // router.use('/comments', commentRoutes);
-// router.use('/properties', propPage);
+router.use('/properties', propPage);
 // router.use('/landlords', landlordPage);
 
 module.exports = router;
